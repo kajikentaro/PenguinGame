3fdfbf3d0e16c202319af4b4789441948ed5e860
kajikentaro <k.kentarou0412@gmail.com>
2021-10-17 19:00:36 +0900

    success to learning env

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #%%
+import numpy as np
 import colorsys
 import tkinter as tk
 import random
@@ -6,56 +7,203 @@ import gym
 from gym import spaces
 import math
 from tkinter.constants import BOTTOM, CENTER
-claee P
-class PenguinGame(gym.Env):
-	CARD_NUM = 7
-	PLAYER_NUM = 4
-	CARDS_CLASS = 5
+import os
+from numpy.lib.function_base import place
+from stable_baselines3 import PPO
+from stable_baselines3.common import env_checker
+from stable_baselines3.common.env_checker import check_env
 
+class Field:
+	CARD_NUM, CARD_SUM, PLAYER_NUM, CARDS_CLASS = None, None, None, None
 
-	selectingCol = None
-	selectingRow = None
-	selectingColor = None
-	playerNo = 0
-	field = None
+	field2d = None
+	field1d = None
 	playerHands = None
-	playerAlive = [True] * PLAYER_NUM
 
+	def __init__(self, CARD_NUM = 7, PLAYER_NUM = 2, CARDS_CLASS = 5):
+		self.CARD_NUM = CARD_NUM
+		self.CARD_SUM = int((1 + CARD_NUM) * CARD_NUM / 2)
+		self.PLAYER_NUM = PLAYER_NUM
+		self.CARDS_CLASS = CARDS_CLASS
+
+		# フィールド初期化
+		self.field1d = [0] * self.CARD_SUM
+		self.field2d = []
+		for i in range(self.CARD_NUM):
+			# -2 が禁止エリア
+			line = [-2] * self.CARD_NUM
+			for j in range(i+1):
+				# -1 が未配置
+				line[j] = -1
+			self.field2d.append(line)
+		# カードを配る
+		cards = []
+		for i in range(self.CARD_SUM):
+			cards.append(i % self.CARDS_CLASS)
+		random.shuffle(cards)
+		self.playerHands = []
+		for i in range(self.PLAYER_NUM):
+			self.playerHands.append([0] * self.CARDS_CLASS)
+		for i in range(self.CARD_SUM):
+			playerNo = i % self.PLAYER_NUM 
+			card = cards[i]
+			self.playerHands[playerNo][card] += 1
+	def placeCard(self, row, col, color, playerNo):
+		if(self.isLegalCardSelect(row, col, color, playerNo) == False):
+			return False
+		else:
+			self.field2d[row][col] = color
+			self.playerHands[playerNo][color] -= 1
+			position = self.rowColToPos(row, col)
+			self.field1d[position] = color + 1
+			return True
+	def getCardSet(self):
+		return self.CARD_NUM, self.CARD_SUM, self.PLAYER_NUM, self.CARDS_CLASS
+	def getObservation(self, playerNo):
+		return np.array(self.field1d + self.playerHands[playerNo])
+
+	def isPlayerAlive(self, playerNo):
+		BLANK = -1
+		for i in range(self.CARD_NUM):
+			for j in range(i+1):
+				if(self.field2d[i][j] != BLANK): continue
+				for k in range(self.CARDS_CLASS):
+					if(self.isLegalCardSelect(i,j,k,playerNo) == False): continue
+					return True
+		return False
+	# ゲーム用関数
+	def isLegalCardSelect(self,row, col, color, playerNo):
+		OUTBOARD = -2
+		BLANK = -1
+		if(row >= self.CARD_NUM or row < 0): return False
+		if(col >= self.CARD_NUM or col < 0): return False
+		if(color >= self.CARDS_CLASS or color < 0): return False
+		# 手持ちのカードがない場合
+		if(self.playerHands[playerNo][color] <= 0): return False
+		# すでにカードがおいてあるか、場外の場合
+		if(self.field2d[row][col] != BLANK): return False
+		# 最下行の場合
+		if(row == self.CARD_NUM - 1):
+			# 端の場合
+			if(col == 0 or col == self.CARD_NUM - 1): return True
+			# 端じゃない場合、側にカードが無いと置けない
+			if(self.field2d[row][col-1] != BLANK or self.field2d[row][col+1] != BLANK): return True
+			return False
+		# 中段の場合、下二枚が埋まっていないとダメ
+		if(self.field2d[row+1][col] == BLANK or self.field2d[row+1][col+1] == BLANK): return False
+		# 中段の場合、そのどちらかの下が同じ色の必要がある
+		if(self.field2d[row+1][col] == color or self.field2d[row+1][col+1] == color): return True
+		else: return False
+	def posToRowCol(self, cardPos):
+		row = int((1 + math.sqrt(8*cardPos)) / 2)
+		col = cardPos - int(row * (row - 1) / 2)
+		return (row, col)
+	def rowColToPos(self, row, col):
+		return int(row * (row - 1) / 2 + col)
+
+	# ランダムにカードを配置する。成功したらTrue、失敗(負けた場合)はFalse
+	def randomSelect(self, playerNo):
+		for i in range(self.CARD_NUM):
+			for j in range(i+1):
+				if(self.field2d[i][j] != 0): continue
+				for k in range(self.CARDS_CLASS):
+					if(self.placeCard(k,i,j,playerNo) == False): continue
+					return True
+		return False
+
+
+class PenguinGame(gym.Env):
+	model = None
+	field = None
+	
+
+
+	def __init__(self):
+		super(PenguinGame, self).__init__()
+		field = Field()
+		CARD_NUM, CARD_SUM, PLAYER_NUM, CARDS_CLASS = field.getCardSet()
+		self.action_space = spaces.MultiDiscrete([CARD_SUM, CARDS_CLASS])
+		self.observation_space = spaces.MultiDiscrete([CARDS_CLASS + 1] * CARD_SUM + [int(CARD_SUM / PLAYER_NUM + 1)] * CARDS_CLASS)
+		if os.path.exists("saved-model"):
+			self.model = PPO.load("saved-model")
+	def reset(self):
+		self.field = Field()
+		return self.field.getObservation(0)
+	def predictEnemy(self, observation):
+		if self.model == None:
+			self.field.randomSelect(1)
+			return True
+		# 5回predictしても配置できなかったらランダムになる
+		for _ in range(5):
+			action, _ = self.model.predict(observation)
+			(row, col) = self.field.posToRowCol(action[0])
+			color = action[1]
+			if self.field.placeCard(row, col, color, 1) == True:
+				return True
+		self.field.randomSelect(1)
+		print("random choice NPC occer")
+			
+
+		
+	def step(self, action):
+		observation, reward, done, info = None, None, False, {}
+		(row, col) = self.field.posToRowCol(action[0])
+		color = action[1]
+		if self.field.placeCard(row, col, color, 0) == False:
+			# 選択に失敗した場合
+			observation = self.field.getObservation(0)
+			reward = -0.01
+			return observation, reward, done, info
+
+		if self.field.isPlayerAlive(1) == False:
+			# 勝ち越し
+			# 渡さなくてもいいTODO
+			observation = self.field.getObservation(0)
+			reward = 1.0
+			done = True
+			return observation, reward, done, info
+		else: 
+			# 敵の番
+			observation_enemy = self.field.getObservation(1)
+			self.predictEnemy(observation_enemy)
+			if self.field.isPlayerAlive(0) == True:
+				# 続行できる場合
+				observation = self.field.getObservation(0)
+				reward = 0.5
+				return observation, reward, done ,info
+			else:
+				# 負けた場合
+				observation = self.field.getObservation(0)
+				reward = 0.5
+				done = True
+				return observation, reward, done, info
+
+	renderMod = None
+	def render(self, mode="console"):
+		if mode != 'console':
+			raise NotImplementedError()
+		if self.renderMod == None:
+			self.renderMod = Render(self.field)
+		self.renderMod.updateDraw()
+		print(self.field.field1d)
+
+class Render:
+	CARD_NUM, CARD_SUM, PLAYER_NUM, CARDS_CLASS = None, None, None, None
+	rectWidth = None
+	rectHeight = None
 	ground = None
 	canvas = None
 	labelText1 = None
 	labelText2 = None
 	labelText3 = None
 
-	rectWidth = int(640 / CARD_NUM)
-	rectHeight = int(480 / CARD_NUM)
-
-	def __init__(self):
-		super(PenguinGame, self).__init__()
-		cardSum = int((1 + self.CARD_NUM) * self.CARD_NUM / 2)
-		self.action_space = spaces.MultiDiscrete([cardSum, self.CARDS_CLASS])
-		self.observation_space = spaces.MultiDiscrete([self.CARDS_CLASS] * (cardSum + int(cardSum / self.PLAYER_NUM + 1)))
-	def reset(self):
-		# フィールド初期化
-		self.field = []
-		for i in range(self.CARD_NUM):
-			line = [-1] * self.CARD_NUM
-			for j in range(i+1):
-				line[j] = 0
-			self.field.append(line)
-		# カードを配る
-		cardSum = int((1 + self.CARD_NUM) * self.CARD_NUM / 2)
-		cards = []
-		for i in range(cardSum):
-			cards.append(i % self.CARDS_CLASS)
-		random.shuffle(cards)
-		self.playerHands = []
-		for i in range(self.PLAYER_NUM):
-			self.playerHands.append([0] * self.CARDS_CLASS)
-		for i in range(cardSum):
-			playerNo = i % self.PLAYER_NUM 
-			card = cards[i]
-			self.playerHands[playerNo][card] += 1
+	field = None
+	def __init__(self, field):
+		self.CARD_NUM, self.CARD_SUM, self.PLAYER_NUM, self.CARDS_CLASS = field.getCardSet()
+		self.rectWidth = int(640 / self.CARD_NUM)
+		self.rectHeight = int(480 / self.CARD_NUM)
+		self.field = field
+		self.initDraw()
 
 	def initDraw(self):
 		# ウィンドウ描写
@@ -88,127 +236,6 @@ class PenguinGame(gym.Env):
 		tk.Label(self.ground, textvariable=self.labelText1).pack(side = tk.TOP)
 		tk.Label(self.ground, textvariable=self.labelText2).pack(side = tk.TOP)
 		tk.Label(self.ground, textvariable=self.labelText3).pack(side = tk.TOP)
-		self.reset()
-		self.updateDraw()
-		self.ground.mainloop()
-	def step(self, action):
-		cardPos = action[0]
-		self.selectingRow = int((1 + math.sqrt(8*cardPos)) / 2)
-		self.selectingCol = cardPos - int(self.selectingRow* (self.selectingRow - 1) / 2)
-		self.selectingColor = action[1] + 1
-		def isPlayerAlive():
-			for i in range(self.CARD_NUM):
-				for j in range(i+1):
-					if(self.field[i][j] != 0): continue
-					for k in range(self.CARDS_CLASS, 0, -1):
-						if(isLegalCardSelect(k,i,j) == False): continue
-						return True
-			return False
-		# ゲーム用関数
-		def isLegalCardSelect(color, row, col):
-			if(row >= self.CARD_NUM or row < 0): return False
-			if(col >= self.CARD_NUM or col < 0): return False
-			if(color > self.CARDS_CLASS + 1 or color < 0): return False
-			if(self.playerHands[self.playerNo][color - 1] <= 0): return False
-			# すでにカードがおいてあるか、場外の場合
-			if(self.field[row][col] != 0): return False
-			# 最下行の場合
-			if(row == self.CARD_NUM - 1):
-				# 端の場合
-				if(col == 0 or col == self.CARD_NUM - 1): return True
-				# 端じゃない場合、側にカードが無いと置けない
-				if(self.field[row][col-1] != 0 or self.field[row][col+1] != 0): return True
-				return False
-			# 中段の場合、下二枚が埋まっていないとダメ
-			if(self.field[row+1][col] == 0 or self.field[row+1][col+1] == 0): return False
-			# 中段の場合、そのどちらかの下が同じ色の必要がある
-			if(self.field[row+1][col] == color or self.field[row+1][col+1] == color): return True
-			else: return False
-		def getResponse():
-			observation, reward, done, info = [], None, False, {}
-			for i in range(self.CARD_NUM):
-				for j in range(i+1):
-					observation.append(self.field[i][j])
-			# カードが正しく選ばれたか検証
-			if(isLegalCardSelect(self.selectingColor, self.selectingRow, self.selectingCol) == False):
-				# wrong selection
-				reward = -0.1
-				return observation + self.playerHands[self.playerNo], reward, done, info
-			# OKな場合は続ける
-			self.field[self.selectingRow][self.selectingCol] = self.selectingColor
-			self.playerHands[self.playerNo][self.selectingColor - 1] -= 1
-			if isPlayerAlive():
-				reward = 0.1
-			else: 
-				reward = 0
-			for _ in range(self.PLAYER_NUM):
-				self.playerNo = (self.playerNo + 1) % self.PLAYER_NUM
-				if(self.playerAlive[self.playerNo] == False): continue
-				if(isPlayerAlive() == True):
-					self.updateDraw()
-					return
-				else:
-					self.playerAlive[self.playerNo] = False
-			self.labelText3.set("ゲームオーバー")
-			return observation + self.playerHands[self.playerNo], reward, done, info
-		return getResponse()
-
-
-	def onClick(self,row = None, col = None, color = None):
-		def isPlayerAlive():
-			for i in range(self.CARD_NUM):
-				for j in range(i+1):
-					if(self.field[i][j] != 0): continue
-					for k in range(self.CARDS_CLASS, 0, -1):
-						if(isLegalCardSelect(k,i,j) == False): continue
-						return True
-			return False
-		# ゲーム用関数
-		def isLegalCardSelect(color, row, col):
-			if(row >= self.CARD_NUM or row < 0): return False
-			if(col >= self.CARD_NUM or col < 0): return False
-			if(color > self.CARDS_CLASS + 1 or color < 0): return False
-			if(self.playerHands[self.playerNo][color - 1] <= 0): return False
-			# すでにカードがおいてあるか、場外の場合
-			if(self.field[row][col] != 0): return False
-			# 最下行の場合
-			if(row == self.CARD_NUM - 1):
-				# 端の場合
-				if(col == 0 or col == self.CARD_NUM - 1): return True
-				# 端じゃない場合、側にカードが無いと置けない
-				if(self.field[row][col-1] != 0 or self.field[row][col+1] != 0): return True
-				return False
-			# 中段の場合、下二枚が埋まっていないとダメ
-			if(self.field[row+1][col] == 0 or self.field[row+1][col+1] == 0): return False
-			# 中段の場合、そのどちらかの下が同じ色の必要がある
-			if(self.field[row+1][col] == color or self.field[row+1][col+1] == color): return True
-			else: return False
-		if(row != None): self.selectingRow = row
-		if(col != None): self.selectingCol = col
-		if(color != None): self.selectingColor = color
-		print(self.selectingCol, self.selectingRow, self.selectingColor)
-		if(self.selectingCol == None or self.selectingRow == None or self.selectingColor == None): return
-		# カードが正しく選ばれたか検証
-		if(isLegalCardSelect(self.selectingColor, self.selectingRow, self.selectingCol) == False):
-			print("wrong selection")
-			self.selectingRow, self.selectingCol, self.selectingColor = None, None, None
-			return
-		# OKな場合は続ける
-		self.field[self.selectingRow][self.selectingCol] = self.selectingColor
-		self.playerHands[self.playerNo][self.selectingColor - 1] -= 1
-		self.selectingRow, self.selectingCol, self.selectingColor = None, None, None
-		print(self.selectingRow, self.selectingCol, self.selectingColor)
-		for _ in range(self.PLAYER_NUM):
-			self.playerNo = (self.playerNo + 1) % self.PLAYER_NUM
-			if(self.playerAlive[self.playerNo] == False): continue
-			if(isPlayerAlive() == True):
-				self.updateDraw()
-				return
-			else:
-				self.playerAlive[self.playerNo] = False
-		self.labelText3.set("ゲームオーバー")
-		
-		
 
 	def updateDraw(self):
 		def hsvToRgb(h,s,v):
@@ -219,7 +246,7 @@ class PenguinGame(gym.Env):
 			return rgbHexStr
 
 		def fieldToRGB(fieldStatus):
-			if(fieldStatus == 0): return "#ddd"
+			if(fieldStatus == -1): return "#ddd"
 			colorRatio = fieldStatus / self.CARDS_CLASS
 			return hsvToRgb(colorRatio, 0.86, 0.9)
 
@@ -229,8 +256,8 @@ class PenguinGame(gym.Env):
 			self.onClick(row = y, col = x)
 
 		# ラベルアップデート
-		self.labelText1.set("player no = " + str(self.playerNo))
-		self.labelText2.set(str(self.playerHands[self.playerNo]))
+		self.labelText1.set(str(self.field.playerHands[0]))
+		self.labelText2.set(str(self.field.playerHands[1]))
 		# キャンバスアップデート
 		self.canvas.delete("all")
 		for i in range(self.CARD_NUM):
@@ -238,10 +265,27 @@ class PenguinGame(gym.Env):
 			y0 = self.rectHeight * i
 			for j in range(i + 1):
 				id = "rect" + str(i * self.CARD_NUM + j)
-				self.canvas.create_rectangle(x0, y0, x0 + self.rectWidth, y0 + self.rectHeight, fill = fieldToRGB(self.field[i][j]), outline = "#000", tags=id)
+				self.canvas.create_rectangle(x0, y0, x0 + self.rectWidth, y0 + self.rectHeight, fill = fieldToRGB(self.field.field2d[i][j]), outline = "#000", tags=id)
 				self.canvas.tag_bind(id, "<ButtonRelease-1>", clickRect,3)
 				x0 += self.rectWidth
+		self.ground.update()
 
-
-main = PenguinGame()
 # %%
+for i in range(15):
+	env = PenguinGame()
+	model = PPO('MlpPolicy', env, verbose=1)
+	if i != 0:
+		model.load("saved-model")
+	model.learn(total_timesteps=25000)
+	model.save("saved-model")
+	print(i)
+
+import time
+obs = env.reset()
+for _ in range(1000):
+		action, _states = model.predict(obs)
+		obs, rewards, dones, info = env.step(action)
+		if dones:
+			break
+		env.render()
+		time.sleep(0.1)
--- test.py
+++ test.py
@@ -1,22 +1,4 @@
-class Child:
-	parentObj = None
-	def __init__(self, parentObj):
-		self.parentObj = parentObj
-		print("I'm a child")
-		parentObj.countUP()
-		print(parentObj.no)
-class Parent:
-	no = 0
-	def __init__(self):
-		print("I'm a parent")
-		child1 = Child(self)
-		child2 = Child(self)
-		child3 = Child(self)
-		child4 = Child(self)
-	def countUP(self):
-		self.no += 1
-		print("now, no is ", self.no)
-		
-
-
-parent = Parent()
+class test:
+	def __init__(self, abc):
+		print(abc)
+aaa = test("abbbb")
